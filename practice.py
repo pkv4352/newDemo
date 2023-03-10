2545f937c303e2d3d826d7551957bf2349096ff0
Pavan <pavankumarvakalapudi1612@gmail.com>
2023-03-10 19:36:40 +0530

    My first Commit

---
+++ practice.py
@@ -0,0 +1,11 @@
+def set_diff(a,b):
+    x = []
+    for i in a:
+        if i not in b:
+            x.append(i)
+    print(x)
+m = int(input("ENter"))
+a = list(map(int,input("\nEnter the numbers : ").strip().split()))[:m]
+n = int(input("ENter"))
+b = list(map(int,input("\nEnter the numbers : ").strip().split()))[:n]
+set_diff(a,b)
